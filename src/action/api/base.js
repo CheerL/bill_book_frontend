df5465634ed7d8a5da977923586adf65e7e0df0c
Cheer.L <lcr19960717@gmail.com>
2019-08-30 01:53:15 +0800

    new base

--- src/action/api/base.js
+++ src/action/api/base.js
@@ -1,4 +1,4 @@
 export default {
-    // dev: 'http://192.168.10.100:5000/api/v1'
-    dev: 'http://localhost:5000/api/v1'
+    dev: 'http://192.168.10.100:5000/api/v1'
+    // dev: 'http://localhost:5000/api/v1'
 }
